8adcc9ae9219f4e5ba7c8dad17259a689cdb776b
djzhao <djzhao627@gmail.com>
2021-10-30 16:54:00 +0800

    更新值注入的方式，兼容性增加

--- app/src/main/java/cn/djzhao/annotationusage/anno/AutowiredUtil.java
+++ app/src/main/java/cn/djzhao/annotationusage/anno/AutowiredUtil.java
@@ -1,34 +1,41 @@
 package cn.djzhao.annotationusage.anno;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 public class AutowiredUtil {
     public static void injectValue(Activity activity) {
-        try {
-            Intent intent = activity.getIntent();
-            Class<? extends Activity> aClass = activity.getClass();
-            for (Field declaredField : aClass.getDeclaredFields()) {
-                declaredField.setAccessible(true);
-                if (declaredField.isAnnotationPresent(Autowired.class)) {
-                    Autowired annotation = declaredField.getAnnotation(Autowired.class);
-                    String value = annotation.value();
-                    Class<?> type = declaredField.getType();
-                    switch (type.toString()) {
-                        case "class java.lang.String":
-                            declaredField.set(activity, intent.getStringExtra(value));
-                            break;
-                        case "int":
-                        case "class java.lang.Integer":
-                            declaredField.set(activity, intent.getIntExtra(value, 0));
-                            break;
+        Bundle extras = activity.getIntent().getExtras();
+        Class<? extends Activity> aClass = activity.getClass();
+        for (Field declaredField : aClass.getDeclaredFields()) {
+            declaredField.setAccessible(true);
+            if (declaredField.isAnnotationPresent(Autowired.class)) {
+                Autowired autowired = declaredField.getAnnotation(Autowired.class);
+                // 获取注解参数名
+                String key = autowired.value().equals("") ? declaredField.getName() : autowired.value();
+                // 当前key在extras中存在
+                if (extras.containsKey(key)) {
+                    Object value = extras.get(key);
+                    // 如果当前属性是数组，则返回数组元素类型，否则返回null
+                    Class<?> componentType = declaredField.getType().getComponentType();
+                    // 如果是Parcelable类型的数组，则需要特殊处理
+                    if (declaredField.getType().isArray() && Parcelable.class.isAssignableFrom(componentType)) {
+                        Object[] objects = (Object[]) value;
+                        // 转为Parcelable[]数组
+                        value = Arrays.copyOf(objects, objects.length, Parcelable[].class);
+                    }
+                    declaredField.setAccessible(true);
+                    try {
+                        declaredField.set(activity, value);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
                     }
                 }
             }
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
         }
     }
 }
